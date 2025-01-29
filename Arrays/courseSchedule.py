82c2a004979d30adc98d46638822f6bb78da1da1
Mikhail Alexeev <mishalxv35@gmail.com>
2025-01-29 12:27:43 -0800

    course sched iv

--- Arrays/courseSchedule.py
+++ Arrays/courseSchedule.py
@@ -26,6 +26,52 @@ def courseScheduleII(numCourses: int, prerequisites: List[List[int]]) -> List[in
     return res if len(res) == numCourses else []
 
 
-print(courseScheduleII(2, [[1, 0]]))
-print(courseScheduleII(1, []))
-print(courseScheduleII(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
+def courseScheduleIV(
+    numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]
+) -> List[bool]:
+    N = len(prerequisites)
+    if N == 0:
+        return [False] * len(queries)
+    # find nodes without prerequisites
+    map = {}
+    for i in range(N):
+        pre_req, course = prerequisites[i]
+        if pre_req in map:
+            map[pre_req].append(course)
+        else:
+            map[pre_req] = [course]
+    # get starting course
+
+    def dfs(course, target, visited):
+        if course == target:
+            return True
+        if course not in map:
+            return False
+        for next in map[course]:
+            if next not in visited:
+                visited.add(next)
+            if dfs(next, target, visited):
+                return True
+        return False
+
+    print(map)
+    res = []
+    for course, target in queries:
+        res.append(dfs(course, target, set([course])))
+    return res
+
+
+# print(courseScheduleII(2, [[1, 0]]))
+# print(courseScheduleII(1, []))
+# print(courseScheduleII(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
+
+print(courseScheduleIV(2, [[1, 0]], [[0, 1], [1, 0]]))
+print(courseScheduleIV(2, [], [[0, 1], [1, 0]]))
+print(courseScheduleIV(3, [[1, 2], [1, 0], [2, 0]], [[1, 0], [1, 2]]))
+print(
+    courseScheduleIV(
+        4,
+        [[2, 3], [2, 1], [0, 3], [0, 1]],
+        [[0, 1], [0, 3], [2, 3], [3, 0], [2, 0], [0, 2]],
+    )
+)
